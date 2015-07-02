0fc9231aa8ee495a344670be3c3015bf76f5597a
k11mamaes <Mason@MasonM.local>
2015-07-02 05:00:25 -1000

    Phonegap June 30 Version
    Before July 1st changes

---
+++ phonegap-06_30_2015/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/AndroidManifest.xml \
---
+++ phonegap-06_30_2015/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/ant-build/classes.dex \
+/Users/Mason/Sites/UserSites/SeniorDesign/ikure-yesterday/phonegap-yesterday/platforms/android/CordovaLib/ant-build/classes.jar \
